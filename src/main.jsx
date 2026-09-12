7dd8b5e1a230c988a3354b8c03d67b775f2fcb9c
Whyteluv <whiteviky1@gmail.com>
2026-09-12 17:53:23 +0100

    Add main component for San-Vika AI Media app

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+      <h1>San-Vika AI Media</h1>
+      <p>Your AI image and video advert generator</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
